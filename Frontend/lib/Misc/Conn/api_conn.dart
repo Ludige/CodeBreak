5fb9b0e911327705a456479a4b1553383e303c5e
Luiggi <luiggi.ms.1@gmail.com>
2024-07-05 19:12:15 -0300

    Added: Profile routes to APIConnections

--- Frontend/lib/Misc/Conn/api_conn.dart
+++ Frontend/lib/Misc/Conn/api_conn.dart
@@ -1,17 +1,22 @@
 import 'dart:convert';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 import '../Models/model_profile.dart';
+import '../token.dart';
 import 'api_routes.dart';
 
 class ApiConnections {
   static final Map<String, String> _headerDefault = {
     'Content-Type': 'application/json; charset=UTF-8',
   };
-  // static final Map<String, String> _headerWithTokenWithTime = {
-  //   'Content-Type': 'application/json; charset=UTF-8',
-  //   'x-access-time': DateTime.now().millisecondsSinceEpoch.toString()
-  // };
+
+  static final Token _token = Get.put(Token());
+
+  static final Map<String, String> _headerWithTokenWithTime = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'x-access-time': DateTime.now().millisecondsSinceEpoch.toString()
+  };
 
   static Future<Profile> createProfile(Profile profile) async {
     final response = await http.post(
@@ -30,8 +35,56 @@ class ApiConnections {
       print(
           "Error ${response.statusCode.toString()}: ${response.body.toString()}");
       return Profile.defaultGetX();
-    }    
+    }
   }
 
+//TODO Fazer a rota de update de perfil, pedir ajuda ao rafael quando ele puder, sem urgencia, não precisa pra entregar
   
+  static Future<bool> followProfile(String profileId) async {
+    final response = await http.post(
+      Uri.parse(APIRoutes.apiUrl + APIRoutes.followProfileById(profileId)),
+      headers: _headerWithTokenWithTime,
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      print(
+          "Error ${response.statusCode.toString()}: ${response.body.toString()}");
+      return false;
+    }
+  }
+
+  static Future<Profile> getProfileById(id) async {
+    final response = await http.get(
+      Uri.parse(APIRoutes.apiUrl + APIRoutes.getProfileById(id)),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-access-token': _token.token.value
+      },
+    );
+    if (response.statusCode == 200) {
+      var profilesMap = jsonDecode(response.body);
+
+      return Profile.fromMap(profilesMap);
+    } else {
+      print(
+          "Error ${response.statusCode.toString()}: ${response.body.toString()}");
+      throw Exception('Falha ao recuperar perfis');
+    }
+  }
+
+static Future<bool> deleteProfile(String id) async {
+    final response = await http.delete(
+        Uri.parse(APIRoutes.apiUrl + APIRoutes.deleteProfileById(id)),
+        headers: _headerWithTokenWithTime);
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      print(
+          "Error ${response.statusCode.toString()}: ${response.body.toString()}");
+          //TODO trow expetion
+      return false;
+    }
+  }
 }
--- Frontend/lib/Misc/Conn/api_routes.dart
+++ Frontend/lib/Misc/Conn/api_routes.dart
@@ -3,24 +3,28 @@ class APIRoutes {
 
 
   static String createProfile = '/profile/create';
-  static String followProfileById = '/profile/follow';
-  static String updateProfileByToken = '/profile/update';
-  static String updateProfileById = '/profile/getProfileByToken';
+  static String updateProfileByToken = '/profile/getProfileByToken'; //TODO implementar
 
-  static String getProfileByIdId(id) {
+  static String getProfileById(id) {
     return '/profile/getById/$id';
   }
 
+  static String followProfileById(followObjectId) {
+    return '/profile/follow/$followObjectId';
+  }
+  
   static String deleteProfileById(id) {
     return '/profile/deleteById/$id';
   }
 
   //Exercices
-  static String getAllExercice = '/exercice/getAll';
+  // static String getAllExercice = '/exercice/getAll';
   
   static String getExerciceByIdId(id) {
     return '/exercice/getById/$id';
   }
+
+  //TODO criar rota de exercicio especifico
 
   //Lesson
   static String getLessonById(id) {
